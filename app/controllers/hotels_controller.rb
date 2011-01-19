ec9be589b890fa5661d0927357c57b7ab60c7a53
Jen-Mei Wu <jenmei@colorxp.com>
2011-01-19 12:57:25 -0800

    Walkthrough 5-3: Adding a filter

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,5 +1,18 @@
 class HotelsController < ApplicationController
+
+  before_filter :load_hotel, :except => [:new, :index, :create]
+  
   def index
     @hotels = Hotel.order('name')
   end
+
+  def show
+  end
+
+  protected
+
+  def load_hotel
+    @hotel = Hotel.find(params[:id])
+  end
+  
 end
