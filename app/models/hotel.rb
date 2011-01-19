47e0fd70e1d8c2e4d4d8b03458f07d00dd907409
Jen-Mei Wu <jenmei@samma-sati.(none)>
2011-01-19 12:09:08 -0800

    Walkthrough 3-4 Validations

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,3 +1,5 @@
 class Hotel < ActiveRecord::Base
+  validates :name, :presence => true
+  validates_length_of :name, :minimum => 2
 end
 
--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -1,9 +1,19 @@
 require 'spec_helper'
 
 describe Hotel do
-  it "should exist" do
-    @hotel = Hotel.new
+  it "should be valid with valid attributes" do
+    @hotel = Hotel.new(:name => "Travelodge")
     @hotel.should be_valid
+  end
+  
+  it "should not be valid without a name" do
+    @hotel = Hotel.new
+    @hotel.should_not be_valid    
+  end
+
+  it "should not be valid with a name shorter than 2 characters" do
+    @hotel = Hotel.new(:name => "A")
+    @hotel.should_not be_valid    
   end
 end
 
