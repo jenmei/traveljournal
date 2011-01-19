ab3277920895260a589c8237bf118c6c07faaff8
Jen-Mei Wu <jenmei@samma-sati.(none)>
2011-01-19 11:45:26 -0800

    initial commit after work done in unit 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Traveljournal::Application.config.secret_token = 'a76eb6165e2aece78fdc8e588505b87632dbc4896261fba1a014fe309701a04d030463d1aba9789d803394ee954093055348a8e631f6c72348dadc994b29fc7e'
---
+++ db/migrate/20110119194130_create_hotels.rb
@@ -0,0 +1,7 @@
+class CreateHotels < ActiveRecord::Migration
+  def self.up
+  end
+
+  def self.down
+  end
+end
