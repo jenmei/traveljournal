b52add924dd10dba2314360ae325b272fe4ef84a
Jen-Mei Wu <jenmei@samma-sati.(none)>
2011-01-19 12:04:50 -0800

    Walkthrough 3-3. Creating Seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110119194709) do
+ActiveRecord::Schema.define(:version => 20110119200146) do
+
+  create_table "cities", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "hotels", :force => true do |t|
     t.string   "name"
