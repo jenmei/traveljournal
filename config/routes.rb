c42d51edfcca3c2f3ed7d8ad3f345be756e83719
Jen-Mei Wu <jenmei@colorxp.com>
2011-01-19 12:41:30 -0800

    Walkthrough 5-1: Creating Routes

--- config/routes.rb
+++ config/routes.rb
@@ -56,4 +56,6 @@ Traveljournal::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
   resources :hotels
+  root :to => "hotels#index"
 end
+
