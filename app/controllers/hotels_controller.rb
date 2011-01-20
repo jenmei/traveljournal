845726898eb11421aa55760267b8e2cfe9894c22
Jen-Mei Wu <jenmei@colorxp.com>
2011-01-19 17:17:58 -0800

    Walkthrough 6-2: Creating a Form

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -10,6 +10,27 @@ class HotelsController < ApplicationController
   def show
   end
 
+  def new
+    @cities = City.order('name')
+    respond_with(@hotel = Hotel.new)
+  end
+
+  def create
+    @hotel = Hotel.new(params[:hotel])
+    if @hotel.save
+      flash[:notice] = "Hotel was created successfully."
+      respond_with(@hotel, :status => :created, :location => @hotel) do |format|
+        format.html { redirect_to @hotel }
+      end
+    else
+      flash[:error] = @hotel.errors.full_messages.join("\n")
+      @cities = City.order('name')
+      respond_with(@hotel.errors, :status => :unprocessable_entity) do |format|
+        format.html { render :action => :new }
+      end
+    end
+  end
+
   protected
 
   def load_hotel
--- spec/integration/hotels_spec.rb
+++ spec/integration/hotels_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe "hotels" do
   before do
+    @detroit = Factory.create(
+      :city,
+      :name => "Detroit"
+    )
     @hotel1 = Factory.create(
       :hotel, 
       :name => "The Lakes"
@@ -70,6 +74,53 @@ describe "hotels" do
         hotels = JSON.parse(page.body)
         hotels.should be_an(Array)
         hotels.first["hotel"]["name"].should == "St. Clair Hotel"
+      end
+    end
+
+  end
+
+  describe "creating new hotels" do
+    before do
+      visit "/hotels/new"
+    end
+
+    it "should present a form for submitting a new hotel" do
+      page.should have_xpath('//h1[.="New Hotel"]')
+      page.should have_xpath('//form/div/label[.="Name"]')
+      page.should have_xpath('//form/div/label[.="Description"]')
+      page.should have_xpath('//form/div/label[.="City"]')
+    end
+
+    describe "when filling in form and submitting" do
+      describe "with valid parameters" do
+        before do
+          fill_in 'Name', :with => "The Lakeside Hotel"
+          fill_in "Description", :with => "A lovely hotel with a view of Lake St. Clair."
+          select "Detroit", :from => 'hotel_city_id'
+          click_button "Save"
+        end
+
+        it "should display list of hotels with our newly created hotel" do
+          page.find('h1').should have_content('The Lakeside Hotel')
+          lakeside_hotel = Hotel.find_by_name("The Lakeside Hotel")
+          current_path.should == hotel_path(lakeside_hotel)
+        end
+
+        it "should associate the correct city with our hotel" do
+          lakeside_hotel = Hotel.find_by_name("The Lakeside Hotel")
+          lakeside_hotel.city.should == @detroit
+        end
+      end
+    end
+
+    describe "with invalid parameters" do
+      before do
+        click_button "Save"
+      end
+
+      it "should redisplay the form w/ an error message" do
+        page.should have_selector('div.error')
+        page.find('div.error').should have_content("Name can't be blank")
       end
     end
 
