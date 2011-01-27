ee0a14272f5ec388cee4046f33f3a6c9c6125b7f
Jen-Mei Wu <jenmei@colorxp.com>
2011-01-27 14:08:27 -0800

    Finished with walkthrough 6-2

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -10,6 +10,23 @@ class HotelsController < ApplicationController
   def show
   end
 
+  def new
+    @cities = City.order('name')
+    respond_with(@hotel = Hotel.new)
+  end
+
+  def create
+    @hotel = Hotel.new(params[:hotel])
+    if @hotel.save
+      redirect_to @hotel
+    else
+      flash[:error] = @hotel.errors.full_messages.join("\n")
+      @cities = City.order('name')
+      @hotel = Hotel.new
+      render :action => :new
+    end
+  end
+
   protected
 
   def load_hotel
--- spec/integration/hotels_spec.rb
+++ spec/integration/hotels_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe "hotels" do
   before do
+    @detroit = Factory.create(
+      :city,
+      :name => "Detroit"
+    )
     @hotel1 = Factory.create( :hotel, :name => "The Lakes" )
     @hotel2 = Factory.create( :hotel, :name => "The St. Clair" )
   end
@@ -61,4 +65,49 @@ describe "hotels" do
     end
   end
 
+  describe "creating new hotels" do
+    before do
+      visit "/hotels/new"
+    end
+
+    it "should present a form for submitting a new hotel" do
+      header = page.find('h1').text
+      header.should == "New Hotel"
+      page.should have_css('form div label:contains("Name")')
+      page.should have_css('form div label:contains("Description")')
+      page.should have_css('form div label:contains("City")')
+    end
+
+    describe "when filling in a form and submitting" do
+      describe "using valid parameters" do
+        before do
+          fill_in 'Name', :with => "The Lakeside Hotel"
+          fill_in 'Description', :with => "A lovely view of Lake St. Clair"
+          select "Detroit", :from => 'hotel_city_id'
+          click_button "Save"
+        end
+
+        it "should display the hotel that we just created" do
+          page.find('h1').should have_content('The Lakeside Hotel')
+          lakeside_hotel = Hotel.find_by_name("The Lakeside Hotel")
+          current_path.should == hotel_path(lakeside_hotel)
+        end
+
+        it "should associate the correct city with our hotel" do
+          lakeside_hotel = Hotel.find_by_name("The Lakeside Hotel")
+          lakeside_hotel.city.should == @detroit
+        end
+      end
+
+      describe "with invalid parameters" do
+        before do
+          click_button "Save"
+        end
+
+        it "should redisplay the form w/ an error message" do
+          page.find('div.error:contains("Name can\'t be blank")')
+        end
+      end
+    end
+  end
 end
